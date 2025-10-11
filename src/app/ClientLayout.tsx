d8420cdb3101bcd64f6c71ab6fa71ac6abc9907c
HOOR ALMASHHOUR <houralmashhour@gmail.com>
2025-10-11 16:17:32 +0300

    edit

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,37 +1,14 @@
 "use client";
-
-import { useState } from "react";
-import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-
-type Product = {
-  title: string;
-  image: string;
-  whatsappNumber: string;
-};
+import Navbar from "./components/Navbar/Navbar";
+import { SearchProvider } from "./context/SearchContext";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchResults, setSearchResults] = useState<Product[]>([]);
-
-  const handleSearch = () => {
-    const storedProducts = JSON.parse(localStorage.getItem("products") || "[]");
-    const results = storedProducts.filter((product: Product) =>
-      product.title.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setSearchResults(results);
-    localStorage.setItem("searchResults", JSON.stringify(results));
-  };
-
   return (
-    <>
-      <Navbar
-        searchTerm={searchTerm}
-        setSearchTerm={setSearchTerm}
-        handleSearch={handleSearch}
-      />
+    <SearchProvider>
+      <Navbar />
       <main>{children}</main>
       <Footer />
-    </>
+    </SearchProvider>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import ClientLayout from "./ClientLayout";
-import { SearchProvider } from "./context/SearchContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,11 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ar">
       <body className={inter.className}>
-       <SearchProvider>
-          <ClientLayout>
-            {children}
-          </ClientLayout>
-        </SearchProvider>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
